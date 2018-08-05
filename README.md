9ec16f22cc7ff69fce20c82e9802818490005e4a
Glauber Mota <glaubernm@gmail.com>
2018-08-05 01:41:15 -0300

    Correções

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Frontend Challenge - Qconcursos
+
+Para rodar o ambiente de desenvolvimento, execute `npm start` na pasta raiz.
+
+Para ver online: [https://glauberm.github.io/qconcursos/](https://glauberm.github.io/qconcursos/)
+
+## Tecnologias utilizadas
+
+- HTML5
+- CSS3
+- JavaScript (ES6)
+- Webpack
+- POSTCSS
+- SASS
+- BrowserSync
--- webpack.config.js
+++ webpack.config.js
@@ -57,22 +57,28 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpg|gif|ico)$/,
         use: [
           'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
         ]
       }
     ]
   },
   'plugins': [
     new CleanWebpackPlugin(
-      ['dist'],
-      {
-        exclude: [
-          'favicon.ico',
-          'manifest.json'
-        ]
-      }
+      ['dist']
     ),
     new MiniCssExtractPlugin({
       'filename': '[name].[chunkhash:8].css',
